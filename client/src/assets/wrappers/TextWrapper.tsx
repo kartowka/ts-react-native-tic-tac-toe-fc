5328fef0f20874942848bba89009f2a965b5a119
kartowka <antonfley@gmail.com>
2022-05-18 17:36:13 +0300

    fixed top text iOS

--- client/src/assets/wrappers/TextWrapper.tsx
+++ client/src/assets/wrappers/TextWrapper.tsx
@@ -1,28 +1,28 @@
 import { StyleSheet } from 'react-native'
 const TextWrapper = StyleSheet.create({
-  container: {
-    margin: 10,
-    color: '#01CBC6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 50,
-  },
-  resetGame: {
-    margin: 30,
-    flexDirection: 'row',
-    padding: 20,
-    backgroundColor: '#01CBC6',
-    width: 200,
-    borderRadius: 20,
-  },
-  resetGameText: {
-    color: '#2B2B52',
-    fontSize: 20,
-  },
-  resetGameIcon: {
-    fontSize: 30,
-    color: '#2B2B52',
-  },
+	container: {
+		margin: 40,
+		color: '#01CBC6',
+		alignItems: 'center',
+		justifyContent: 'center',
+		fontSize: 50,
+	},
+	resetGame: {
+		margin: 30,
+		flexDirection: 'row',
+		padding: 20,
+		backgroundColor: '#01CBC6',
+		width: 200,
+		borderRadius: 20,
+	},
+	resetGameText: {
+		color: '#2B2B52',
+		fontSize: 20,
+	},
+	resetGameIcon: {
+		fontSize: 30,
+		color: '#2B2B52',
+	},
 })
 
 export default TextWrapper
