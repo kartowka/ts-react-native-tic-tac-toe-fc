26f157fc0b3cbab9f37907c6156ab0b1ed82aed6
kartowka <antonfley@gmail.com>
2022-05-18 16:05:18 +0300

    instead of using import module from you can import few from folder

---
+++ client/src/assets/wrappers/index.tsx
@@ -0,0 +1,7 @@
+import BoardWrapper from './BoardWrapper'
+import RowWrapper from './RowTileWrapper'
+import TileWrapper from './TileWrapper'
+import PlayerWrapper from './PlayerWrapper'
+import TextWrapper from './TextWrapper'
+
+export { BoardWrapper, RowWrapper, TileWrapper, PlayerWrapper, TextWrapper }
