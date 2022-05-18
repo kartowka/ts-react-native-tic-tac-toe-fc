0973e33ddc7c0e3d04bbfdf3a3479f2a36f085a2
kartowka <antonfley@gmail.com>
2022-05-18 16:05:42 +0300

    player style presented here

---
+++ client/src/assets/wrappers/PlayerWrapper.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+const PlayerWrapper = StyleSheet.create({
+  playerX: {
+    color: '#FF3031',
+    fontSize: 70,
+    margin: 5,
+  },
+  playerO: {
+    color: '#45CE30',
+    fontSize: 70,
+    margin: 5,
+  },
+  question: {
+    color: '#74B9FF',
+    fontSize: 70,
+    margin: 5,
+  },
+})
+
+export default PlayerWrapper
