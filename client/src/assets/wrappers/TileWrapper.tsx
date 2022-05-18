b6f4cbe76847b757c99243aaf9934f543a9056d1
kartowka <antonfley@gmail.com>
2022-05-18 16:06:23 +0300

    tile style

---
+++ client/src/assets/wrappers/TileWrapper.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const TileWrapper = StyleSheet.create({
+  tile: {
+    borderColor: '#000000',
+    borderWidth: 4,
+    width: 100,
+    height: 100,
+  },
+})
+
+export default TileWrapper
