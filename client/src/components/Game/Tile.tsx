760b82fb1dffb692f303d4068b5643535a709578
kartowka <antonfley@gmail.com>
2022-05-19 15:59:42 +0300

    change renderSymbol to inline fn

--- client/src/components/Game/Tile.tsx
+++ client/src/components/Game/Tile.tsx
@@ -3,18 +3,15 @@ import { TouchableOpacity } from 'react-native'
 import { tile, tileSymbol } from '../types'
 
 const Tile: FC<{ props: tile }> = ({ props }) => {
-  const [Symbol, setSymbol] = useState<tileSymbol>({
-    sign: props.symbol.sign,
-    name: props.symbol.name,
-  })
-  const renderSymbol = () => {
-    setSymbol(Symbol => (Symbol = props.symbol))
-    props.clickEvent(props.index)
-  }
-  return (
-    <TouchableOpacity disabled={props.tileState} onPress={renderSymbol}>
-      {<props.symbol.sign />}
-    </TouchableOpacity>
-  )
+	const [Symbol, setSymbol] = useState<tileSymbol>({
+		sign: props.symbol.sign,
+		name: props.symbol.name,
+	})
+	const renderSymbol = () => (setSymbol((Symbol) => (Symbol = props.symbol)), props.clickEvent(props.index))
+	return (
+		<TouchableOpacity disabled={props.tileState} onPress={renderSymbol}>
+			{<props.symbol.sign />}
+		</TouchableOpacity>
+	)
 }
 export default Tile
